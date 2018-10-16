f6990ef8120563ad1692278db2cda35f81ec503d
zlh <17612235899@163.com>
2018-10-16 20:24:39 +0800

    修改tab

--- js/tab.js
+++ js/tab.js
@@ -1,16 +1,16 @@
 var TABHTML = '';
     TABHTML += '<div class="am-g" style="height:50px;background:#f6f6f6;border-top:1px solid #dddddd;position:fixed;bottom:0px;left:0;width:100%;z-index:9999" id="qj_tab">';
-    TABHTML += '<div class="am-u-sm-4" style="text-align:center;color:#484848;font-size: 14px;">';
+    TABHTML += '<div class="am-u-sm-4" style="text-align:center;color:#484848;font-size: 14px;" onclick="GOTOINDEX();">';
     TABHTML += '<p style="margin:0;height:28px;"><i class="iconfont tabIndex" style="font-size:22px;color:#484848;">&#xe60c;</i></p>';
-    TABHTML += '<p style="margin:0;color:#484848;font-size: 14px;" class="tabIndex" onclick="GOTOINDEX();">首页</p>';
+    TABHTML += '<p style="margin:0;color:#484848;font-size: 14px;" class="tabIndex" >首页</p>';
     TABHTML += '</div>';
-    TABHTML += '<div class="am-u-sm-4" style="text-align:center;color:#484848;">';
+    TABHTML += '<div class="am-u-sm-4" style="text-align:center;color:#484848;" onclick="GOTOTOURONGBAO();">';
     TABHTML += '<p style="margin:0;height:28px;"><i class="iconfont tabTou" style="font-size:22px;color:#484848;">&#xe616;</i></p>';
-    TABHTML += '<p style="margin:0;color:#484848;font-size: 14px;" class="tabTou" onclick="GOTOTOURONGBAO();">投融宝</p>';
+    TABHTML += '<p style="margin:0;color:#484848;font-size: 14px;" class="tabTou" >投融宝</p>';
     TABHTML += '</div>';
-    TABHTML += '<div class="am-u-sm-4" style="text-align:center;color:#484848;">';
+    TABHTML += '<div class="am-u-sm-4" style="text-align:center;color:#484848;" onclick="GOTOMY();">';
     TABHTML += '<p style="margin:0;height:28px;"><i class="iconfont tabMy" style="font-size:22px;color:#484848;">&#xe627;</i></p>';
-    TABHTML += '<p style="margin:0;color:#484848;font-size: 14px;" class="tabMy" onclick="GOTOMY();">我的</p>';
+    TABHTML += '<p style="margin:0;color:#484848;font-size: 14px;" class="tabMy" >我的</p>';
     TABHTML += '</div>';
     TABHTML += '</div>';
 
